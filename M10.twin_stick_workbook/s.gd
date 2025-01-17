5d1e49676be09a0c46968b749d1a010c7f97e696
TDMXFFTF <vihuang8@s.sfusd.edu>
2025-01-17 11:24:05 -0800

    Bullet shoot

--- M10.twin_stick_workbook/s.gd
+++ M10.twin_stick_workbook/s.gd
@@ -1,17 +1,11 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-@export var max_speed = 600.0
-@export var acceleration := 1000.0
-@export var deceleration := 800.0
-
-
+@export var speed := 460.0
+@export var drag_factor := 10.0
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var has_input_direction := direction.length() > 0.0
-	var desiredvelocity = direction * max_speed
-	if has_input_direction:
-		velocity = velocity.move_toward(desiredvelocity, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+	var move_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var desired_velocity := speed * move_direction
+	var steering := desired_velocity - velocity
+	velocity += steering * drag_factor * delta
 	move_and_slide()
